8c785d1d818cb724235278db981f17a5098d3f74
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-24 18:50:11 -0300

    add global.json

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adcd6590407f1407865d604aa7e337878f44741c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93596c43dcdf92a3f3ae6da21f2864b1bf60bcc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

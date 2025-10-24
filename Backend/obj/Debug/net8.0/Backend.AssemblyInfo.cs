04e306cf32254b826da4b091d0b8f3c89ff83bba
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-24 17:41:45 -0300

    Adicionando o gitnore

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3778416eebfe3aa66d035dacedf30b600d291897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adcd6590407f1407865d604aa7e337878f44741c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

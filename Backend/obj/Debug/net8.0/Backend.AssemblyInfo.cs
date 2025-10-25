1d8a4812a6bac6e17dd6398e24f26f19eebb931e
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-25 09:47:30 -0300

    Arquivos Frontend organizados para nuvem

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc82bb1324ffe484ed6b34315b9be067cbcf7d4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a643062f5cb3eda460c1991cc2797f1edbe46e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

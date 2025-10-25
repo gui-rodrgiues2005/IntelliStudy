e231febf1260e4f158487d8bd3dffd6c8d7ad701
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-25 08:31:02 -0300

    Hospedando na nuvem

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4235edbc9abe675df72f35b5c625df7689acf6b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc82bb1324ffe484ed6b34315b9be067cbcf7d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

1d13f054371c68d7d30c5025d511f60c32a20a7d
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-26 19:15:54 -0300

    integrando stripe

---
+++ Backend/Migrations/20251026221509_CreatePagamentosCartao.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePagamentosCartao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PagamentosPix");
+
+            migrationBuilder.CreateTable(
+                name: "PagamentosCartao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    TransactionId = table.Column<string>(type: "text", nullable: false),
+                    Valor = table.Column<decimal>(type: "numeric", nullable: false),
+                    Metodo = table.Column<string>(type: "text", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Pago = table.Column<bool>(type: "boolean", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PagamentosCartao", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PagamentosCartao");
+
+            migrationBuilder.CreateTable(
+                name: "PagamentosPix",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CriadoEm = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Pago = table.Column<bool>(type: "boolean", nullable: false),
+                    PixCopiaECola = table.Column<string>(type: "text", nullable: false),
+                    QrCodeUrl = table.Column<string>(type: "text", nullable: false),
+                    Txid = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Valor = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PagamentosPix", x => x.Id);
+                });
+        }
+    }
+}

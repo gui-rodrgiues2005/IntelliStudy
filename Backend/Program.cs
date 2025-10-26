0455424aac6ab91f3afea54c238b093a170a0c74
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-26 20:06:21 -0300

    integrando stripe

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -71,6 +71,7 @@ builder.Services.AddAuthentication(options =>
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 })
+
 .AddJwtBearer(options =>
 {
     options.RequireHttpsMetadata = false;
--- Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3778416eebfe3aa66d035dacedf30b600d291897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a643062f5cb3eda460c1991cc2797f1edbe46e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

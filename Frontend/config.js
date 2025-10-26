fe82133262f952d03670afafa35a6df15e107525
Guilherme Rodrigues Costa <rodriguesguidev.com>
2025-10-26 18:52:04 -0300

    integrando stripe

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = import.meta.env.VITE_API_URL || "https://backend-production-69f3.up.railway.app";
-export const API_URL = "http://localhost:5051";
+export const API_URL = import.meta.env.VITE_API_URL || "https://backend-production-69f3.up.railway.app";
+//export const API_URL = "http://localhost:5051";
